b5a1b39d60d5eda565a69a2c5e4be4f7e9c898f9
Reactongraph <js.developer@ongraph.com>
2019-02-08 11:46:49 +0530

    [feat] facebook login

--- src/component/Button/index.js
+++ src/component/Button/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 
-const Button = (props) => {
-  return (
-    <TouchableOpacity onPress={props.onPress} style={props.style}>
-      <Text style={props.buttonTextStyle}>{props.text}</Text>
-    </TouchableOpacity>
-  );
-}
+const Button = props => (
+  <TouchableOpacity onPress={props.onPress} style={props.style}>
+    <Text style={props.buttonTextStyle}>{props.text}</Text>
+  </TouchableOpacity>
+);
 export default Button;
--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,7 +1,9 @@
-import React, { Component } from 'react'
-import { Text, View, TouchableOpacity, Alert } from "react-native";
+import React, { Component } from 'react';
+import {
+  View, Alert,
+} from 'react-native';
+import Button from 'src/component/Button';
 import styles from './styles';
-import Button from 'src/component/Button'
 
 export default class Dashboard extends Component {
   static navigationOptions = {
@@ -12,7 +14,7 @@ export default class Dashboard extends Component {
     try {
       const {
         type,
-        token
+        token,
       } = await Expo.Facebook.logInWithReadPermissionsAsync('804603689938210', {
         permissions: ['public_profile'],
       });
@@ -20,9 +22,7 @@ export default class Dashboard extends Component {
         // Get the user's name using Facebook's Graph API
         const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
         Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
-        console.log("response", response);
-        console.log("response.json()", response.json());
-
+        console.log('response.json()', response.json());
       } else {
         // type === 'cancel'
       }
